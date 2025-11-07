071f452211c19d950a513f6f8ad756c37c367e87
RM1505 <rigel.mummers@gmail.com>
2025-11-07 09:28:31 -0800

    feat: moved  reused code in scrapers to a core file

---
+++ scraper_core.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+
+def link2soup(url):
+    response = requests.get(url)
+    return BeautifulSoup(response.text, 'html.parser')
+
+class CreditCard:
+    def __init__(self, name, annual_fee, welcome_amount, apr, rewards):
+        self.name = name
+        self.annual_fee = annual_fee
+        self.welcome_amount = welcome_amount
+        self.apr = apr
+        self.rewards = rewards
+        
+    def __repr__(self):
+        return f"CreditCard(name = {self.name},\n annual_fee = {self.annual_fee},\n welcome_amount = {self.welcome_amount},\n apr = {self.apr},\n rewards = {self.rewards})"
