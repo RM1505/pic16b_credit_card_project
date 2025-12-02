fa13fca358eb19f36d7be4ef535276507df2560e
RM1505 <rigel.mummers@gmail.com>
2025-12-02 12:03:14 -0800

    feat:scraper_core.py

--- scraper_core.py
+++ scraper_core.py
@@ -4,13 +4,3 @@ import requests
 def link2soup(url):
     response = requests.get(url)
     return BeautifulSoup(response.text, 'html.parser')
-
-class CreditCard:
-    def __init__(self, name, annual_fee, rewards, issuer = None):
-        self.issuer = issuer
-        self.name = name
-        self.annual_fee = annual_fee
-        self.rewards = rewards
-        
-    def __repr__(self):
-        return f"CreditCard(issuer = {self.issuer} \n, name = {self.name},\n annual_fee = {self.annual_fee}, \n rewards = {self.rewards})"
